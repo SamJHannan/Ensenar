e040850b120ddfb3fd35ed84d0b6791094b5c3f6
NPadrutt <nino.padrutt@hotmail.com>
2015-02-23 21:14:04 +0100

    add csv reader

---
+++ Src/Ensenar.Business/Models/CsvRow.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Ensenar.Business.Models {
+    /// <summary>
+    ///     Class to store one CSV row
+    /// </summary>
+    public class CsvRow : List<string> {
+        public string LineText { get; set; }
+    }
+}
