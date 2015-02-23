18da2675cdf917aed9480dda99bd301743bfa610
NPadrutt <nino.padrutt@hotmail.com>
2015-02-23 21:16:38 +0100

    add unit tests project, code formtaing

---
+++ Src/Ensenar.Business.Test/UnitTest1.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+
+namespace Ensenar.Business.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void HelloWorld()
+        {
+        }
+    }
+}
--- Src/Ensenar.Business/Helper/CsvReader.cs
+++ Src/Ensenar.Business/Helper/CsvReader.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Text;
 using Ensenar.Business.Models;
 
 namespace Ensenar.Business.Helper {
